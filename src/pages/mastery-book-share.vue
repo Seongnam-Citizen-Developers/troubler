9d08f754cb392a7791d563ea787e1a102d29482f
naram.dash <juho_kim@outlook.com>
2021-12-19 18:22:59 +0900

    base work mastery-book-share

--- src/pages/mastery-book-share.vue
+++ src/pages/mastery-book-share.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const code = computed({
+    get: () => route.params.code as string,
+    set: () => { }
+})
+
+
 </script>
 
 <template>
     <h1>특성판 공유</h1>
+
+    <h2>{{ code }}</h2>
 </template>
 
 <style>
