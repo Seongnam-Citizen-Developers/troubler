f489d08d9e24b9855e22d1fc892561d4ec827eb2
naram.dash <juho_kim@outlook.com>
2021-12-19 18:32:50 +0900

    apply tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
+import './index.css'
 
 const app = createApp(App)
 app.use(Router)
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Troubler</h1>
+    <h1 class="text-3xl font-bold underline">Troubler</h1>
     <p>Community Service for TROUBLESHOOTER</p>
 
     <router-link :to="{ name: 'masteryBookShare' }">특성판 공유</router-link>
