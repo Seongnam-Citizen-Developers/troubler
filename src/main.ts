ef6f281ee9165a159b38cc77ed13709decc76b73
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2022-01-13 22:11:47 +0900

    1. dev add : vue-meta 2. vue-meta 연구

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import Router from './router'
+import { createMetaManager } from 'vue-meta'
+
 import './index.css'
 // 처음 create할 때 언어를 kr로 설정
 const i18n = createI18n({ locale: 'kr' })
-// main ts에서 미들웨어로 셋팅해주고, config에서 path설정도 해줘야한다
+
+//   main ts에서 미들웨어로 셋팅해주고, config에서 path설정도 해줘야한다
 const app = createApp(App)
 app.use(i18n)
 app.use(Router)
+app.use(createMetaManager())
+
 app.mount('#app')
